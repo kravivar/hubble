dcba5b167e0931cf5f6ba4d72956e83c0d66d76e
kravivar <rk.kripa@gmail.com>
2017-02-23 13:59:55 -0800

    Update start_hubble.sh

--- start_hubble.sh
+++ start_hubble.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /tmp/hubble-glibc-2.17.tar.gz ]
 then
-  wget https://github.com/hubblestack/hubble/releases/download/v2.1.0/hubble-glibc-2.17.tar.gz
+  wget --no-check-certificate https://github.com/hubblestack/hubble/releases/download/v2.1.0/hubble-glibc-2.17.tar.gz
 fi
 
 /opt/hubble/hubble 
